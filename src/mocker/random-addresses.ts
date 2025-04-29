44c317e9e7ca839521296f2c06bea2ee0550337c
Ironboy <ironboy@users.noreply.github.com>
2025-04-30 00:18:20 +0200

    no

--- src/mocker/random-addresses.ts
+++ src/mocker/random-addresses.ts
@@ -1,6 +1,7 @@
-import data from './data.gzip?inline';
+import data from './data.gzip?raw';
 import pako from 'pako';
 
+console.log(data);
 
-let h = pako.inflate(data);
-console.log(h);
+//let h = pako.inflate(data);
+//console.log(h);
