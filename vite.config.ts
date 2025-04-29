a754a6e07c091d39f9d18145179a311b76e05c0e
Ironboy <ironboy@users.noreply.github.com>
2025-04-29 23:48:44 +0200

    ok

--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,5 @@
-import { defineConfig, ViteDevServer } from "vite";
-
-function serverPlugin() {
-  return {
-    name: 'server-plugin',
-    configureServer(server: ViteDevServer) {
-      server.middlewares.use((req, res, next) => {
-        if (req.url!.endsWith('.wasm')) {
-          res.setHeader('Content-Type', 'application/wasm');
-        }
-        next();
-      });
-    }
-  };
-}
+import { defineConfig } from "vite";
 
 export default defineConfig({
-  assetsInclude: ['**/*.brotli'],
-  plugins: [serverPlugin()]
+  assetsInclude: ['**/*.gzip']
 });
