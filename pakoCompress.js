6e7f9b85e62370d12477836b4dc4344d4e845eba
Ironboy <ironboy@users.noreply.github.com>
2025-04-30 00:03:19 +0200

    ok

---
+++ pakoCompress.js
@@ -0,0 +1,11 @@
+import pako from 'pako';
+import fs from 'fs';
+import path from 'path';
+
+
+
+
+let data = fs.readFileSync(path.join(import.meta.dirname, 'data-uncompressed.json'), 'utf-8');
+data = pako.deflate(data);
+
+fs.writeFileSync(path.join(import.meta.dirname, 'data.pako'), data, 'utf-8');
