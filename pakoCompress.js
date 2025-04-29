94ad9e2d0d6dd3c53250cb5c4fb17cefca274c41
Ironboy <ironboy@users.noreply.github.com>
2025-04-30 00:27:27 +0200

    good

--- pakoCompress.js
+++ pakoCompress.js
@@ -1,9 +1,11 @@
 import pako from 'pako';
 import fs from 'fs';
 import path from 'path';
+import 'es-arraybuffer-base64/auto';
 
 
 let data = fs.readFileSync(path.join(import.meta.dirname, 'data-uncompressed.json'), 'utf-8');
 data = pako.deflate(data);
+console.log(data.toBase64());
 
-fs.writeFileSync(path.join(import.meta.dirname, 'data.gzip'), data);
+fs.writeFileSync(path.join(import.meta.dirname, 'data.gzip.base64'), data, 'utf-8');
